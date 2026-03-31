715670a9b35edd43b8867f882321c0cfbf56f7a5
AyanDutta521 <ayandutta521@gmail.com>
2026-03-31 21:15:46 +0530

    Before Pages are made

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,19 @@ import Footer from './Footer/Footer';
 import Container from './container/Container';
 import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn';
+import Button from './Button';
+import Input from './Input';    
+import Select from './Select';   
+import RTE from './RTE';   
 export {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    RTE
+
 }
